5472120c5a6c213cc2273b654cb3b67a3bccb7ab
SreylenHuon <hounsreylen53@gamil.com>
2025-02-21 22:14:15 +0700

    first commit

---
+++ test.js
@@ -0,0 +1,42 @@
+// let userInout = "r";
+// switch(userInout.toLowerCase()){
+//      case "yes":
+//      console.log("You said yes");
+//      break;
+//      case "no":
+//      console.log("You said no");
+//      break;
+//      default:
+//      console.log("Please enter 'yes' or 'not'.");
+//         break;
+// }
+var userInput = "no";
+if(userInout = "yes"){
+    console.log("U said yes");
+}else if(userInout = "no"){
+    console.log("U said no");
+}else{
+    console.log("U don't said yes or no");
+}
+let a = true;
+let b = true;
+ let sum = a+b;
+console.log(sum);
+
+// tes for each use with obj
+i = 3;
+var person ={ fname:"jonh" , lname:"Doe", age:25};
+for (var x in person ){
+
+    console.log(`Person info ${person[x]}`);
+    // console.log(person);
+}
+console.log(person.fname);
+
+
+// var fname="jonh";
+// var lname="Son";
+// let age=20;
+//  person = ' ${fanme} ${lname} ${age}'
+
+// try catch & finally
